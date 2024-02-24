d46fee45a3cd0e852f3a96f5628fd6dff03b9c28
Лысенко Евгений <evgeen@yandex.ru>
2024-02-24 23:55:22 +0300

    Add removeEmptyDirs()

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async.series([
         console.error('runTests error: %j', err)
       if (res)
         console.log('runTests res: %j', res)
-      // console.timeEnd('runTests')
+      removeEmptyDirs(OUT_DIR)
     })
   }
 })
@@ -225,4 +225,24 @@ function pretty(obj) {
     return val
   }
   console.log(stringify(obj, { space: 2, replacer }))
+}
+
+function removeEmptyDirs(dir) {
+  let isEmpty = true
+  const list = fs.readdirSync(dir, { withFileTypes: true })
+
+  const dirs = list.filter(de => de.isDirectory())
+    .map(de => path.join(de.path, de.name))
+  const files = list.filter(de => de.isFile())
+    .map(de => path.join(de.path, de.name))
+
+  if (files.length) isEmpty = false
+  dirs.forEach(d => {
+    if (!removeEmptyDirs(d))
+      isEmpty = false
+  })
+  if (isEmpty) {
+    fs.rmdirSync(dir)
+  }
+  return isEmpty
 }
