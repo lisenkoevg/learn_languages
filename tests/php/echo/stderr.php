635e6b59faedb2e742c01d06e124dd83187dc86f
Лысенко Евгений <evgeen@yandex.ru>
2024-03-04 08:52:34 +0300

    Fix: php write to stderr + error_log

--- tests/php/echo/stderr.php
+++ tests/php/echo/stderr.php
@@ -1,5 +1,8 @@
 <?php
 // #stderr
-if (ini_get('error_log') == '')
-  error_log('line written to stderr');
+
+$old_error_log = ini_get('error_log');
+ini_set('error_log', '');
+error_log('line written to stderr');
+ini_set('error_log', $old_error_log);
 
